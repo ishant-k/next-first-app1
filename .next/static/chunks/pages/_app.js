b7c88535782e53e0a7822ab4a867325a8642efde
ishant-k <74232790+ishant-k@users.noreply.github.com>
2020-12-15 13:37:07 +0530

    ..

--- .next/static/chunks/pages/_app.js
+++ .next/static/chunks/pages/_app.js
@@ -9041,7 +9041,7 @@ module.exports = function(module) {
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! next-client-pages-loader?page=%2F_app&absolutePagePath=next%2Fdist%2Fpages%2F_app! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_app&absolutePagePath=next%2Fdist%2Fpages%2F_app!./");
-module.exports = __webpack_require__(/*! D:\SampleProjects\SEOPoC\next-first-app\node_modules\next\dist\client\router.js */"./node_modules/next/dist/client/router.js");
+module.exports = __webpack_require__(/*! D:\SampleProjects\SEOPoC\next-first-app-Copy\node_modules\next\dist\client\router.js */"./node_modules/next/dist/client/router.js");
 
 
 /***/ })
